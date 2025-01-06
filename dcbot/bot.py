fd3167db1099124ebbf65b28518d88cddab52985
Tianyu Chen <sweetyfish@deepin.org>
2025-01-06 18:09:37 +0800

    Refactor bot.py

--- dcbot/bot.py
+++ dcbot/bot.py
@@ -58,15 +58,25 @@ class DCBot:
     async def message_callback(self, room: MatrixRoom, event: Event) -> None:
         assert self.client
         assert isinstance(event, RoomMessageText)
-        # deepin-sysdev-team
-        if room.room_id != "!arcYMpuEJhIvmonMaG:matrix.org":
-            return
-        if event.sender not in self.REQUESTER_MAP:
-            logger.info("Unauthorized sender: %s", event.sender)
-            return
-        requester = self.REQUESTER_MAP[event.sender]
 
         event_id = event.event_id
+
+        if event.body[0] != "/":
+            logger.info(
+                "Not a command, ignored. Event ID: %s, Sender: %s",
+                event_id,
+                event.sender,
+            )
+            return
+
+        # deepin-sysdev-team
+        if room.room_id != "!arcYMpuEJhIvmonMaG:matrix.org":
+            logger.info(
+                "Not in deepin-sysdev-team room, ignored. Event ID: %s, Sender: %s",
+                event_id,
+                event.sender,
+            )
+            return
 
         logger.info(
             "Message received in room %s (%s)\n%s (%s) | %s",
@@ -77,55 +87,79 @@ class DCBot:
             event.body,
         )
 
-        if event.body[0] != "/":
-            logger.info(
-                "Not a command, ignored. Event ID: %s, Sender: %s",
-                event.event_id,
-                event.sender,
-            )
-
         command = event.body.split()[0]
         # (package, branch, github_project_name, requester)
         packages = []
         topic = ""
 
-        if command in ["/update", "/batchupdate"]:
-            MAX_RETRY_COUNT = 3
-            for retry_count in range(MAX_RETRY_COUNT + 1):
-                if not os.path.exists("LAST-UPDATED"):
-                    # All retries failed
-                    if retry_count == MAX_RETRY_COUNT:
-                        # LAST-UPDATED still does not present
-                        await self.client.room_send(
-                            room.room_id,
-                            message_type="m.room.message",
-                            content={
-                                "msgtype": "m.text",
-                                "body": "LAST-UPDATED not found.",
-                                "m.relates_to": {
-                                    "m.in_reply_to": {"event_id": event_id}
-                                },
-                            },
-                        )
-                        return
+        if command not in {"/update", "/batchupdate"}:
+            logger.info(
+                "Unsupported command: %s. Event ID: %s, Sender: %s",
+                command,
+                event_id,
+                event.sender,
+            )
+            return
 
-                    # sleep and retry
-                    await asyncio.sleep(5)  # in seconds
-                    continue
+        if event.sender not in self.REQUESTER_MAP:
+            logger.info("Unauthorized sender: %s", event.sender)
+            await self.client.room_send(
+                room_id=room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": "Unauthorized.\nPlease add yourself to "
+                    + "https://github.com/UTsweetyfish/dcbot/blob/main/dcbot/bot.py",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+            return
 
-            if int(open("LAST-UPDATED").read().strip()) < time.time() - 2 * 60 * 60:
-                # LAST-UPDATED fails more than 1 hours ago
-                # Is apt-get down?
-                await self.client.room_send(
-                    room.room_id,
-                    message_type="m.room.message",
-                    content={
-                        "msgtype": "m.text",
-                        "body": "LAST-UPDATED fails more than 2 hours ago. Is apt-get down?",
-                        "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
-                    },
-                )
-                return
+        requester = self.REQUESTER_MAP[event.sender]
+
+        # check if event is already processed
+        if await already_processed(event_id):
+            logger.info("Event %s already processed. Skipping.", event_id)
+            return
+
+        # check LAST-UPDATED
+        MAX_RETRY_COUNT = 3
+        for _ in range(MAX_RETRY_COUNT):
+            if not os.path.exists("LAST-UPDATED"):
+                # sleep and retry
+                await asyncio.sleep(5)  # in seconds
+
+        # All retries failed, LAST-UPDATED still not exist
+        if not os.path.exists("LAST-UPDATED"):
+            # LAST-UPDATED still does not present
+            await self.client.room_send(
+                room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": "LAST-UPDATED not found.",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+            return
+
+        if int(open("LAST-UPDATED").read().strip()) < time.time() - 2 * 60 * 60:
+            # LAST-UPDATED fails more than 1 hours ago
+            # Is apt-get down?
+            await self.client.room_send(
+                room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": "LAST-UPDATED fails more than 2 hours ago. Is apt-get down?",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+            return
+
+        # /update pkg
+        # /batchupdate topic-xxx pkg1 pkg2 ...
+        args = event.body.split()
 
         match command:
             case "/update":
@@ -134,115 +168,111 @@ class DCBot:
                 # requester
                 # do_update()
                 # /update package
-                args = event.body.split()
                 if len(args) != 2:
-                    # TODO:
+                    logger.info("/update should only have one argument. Skipping.")
                     return
                 topic = ""
-                packages.append(args[1])
+                packages = [args[1]]
             case "/batchupdate":
                 # topic, [package, [package, [package, [package, ...]]]]
-                args = event.body.split()
                 # /update topic-xxx pkg1 pkg2
                 if len(args) < 3:
-                    # TODO:
+                    logger.info(
+                        "/batchupdate should have at least topic-xxx and pkg1. Skipping."
+                    )
                     return
                 topic = args[1]
-                packages += args[2:]
+                packages = args[2:]
 
-        if await already_processed(event_id):
-            logger.info("Event %s already processed. Skipping.", event_id)
-            return
-        if packages:
-            if topic:
-                if not topic.startswith("topic-"):
-                    await self.client.room_send(
-                        room.room_id,
-                        message_type="m.room.message",
-                        content={
-                            "msgtype": "m.text",
-                            "body": "Topic name should prefix with topic-.",
-                            "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
-                        },
-                    )
-                    return
-
-                if not validate_topicname(topic):
-                    await self.client.room_send(
-                        room.room_id,
-                        message_type="m.room.message",
-                        content={
-                            "msgtype": "m.text",
-                            "body": "Topic name invalid.",
-                            "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
-                        },
-                    )
-                    return
-
-            results: list[Process] = []
-            for package in packages:
-                logger.info("Updating %s", package)
-                p = await asyncio.create_subprocess_exec(
-                    "python",
-                    "-m",
-                    "dcbot.update",
-                    package,
-                    topic,
-                    "",
-                    requester,
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE,
-                )
-                results.append(p)
-
-            success_count = 0
-
-            for result in results:
-                await result.wait()
-                logger.info(result)
-                logger.info(type(result))
-                if result.returncode == 0:
-                    success_count += 1
-                else:
-                    if result.stderr:
-                        logger.error(await result.stderr.read())
-
-            await mark_processed(event_id)
-
-            if success_count == 0:
+        if topic:
+            if not topic.startswith("topic-"):
                 await self.client.room_send(
                     room.room_id,
                     message_type="m.room.message",
                     content={
                         "msgtype": "m.text",
-                        "body": "All failed 😭😭😭",
+                        "body": "Topic name should prefix with topic-.",
                         "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
                     },
                 )
-            elif success_count < len(results):
-                # 😶😶😶
+                return
+
+            if not validate_topicname(topic):
                 await self.client.room_send(
                     room.room_id,
                     message_type="m.room.message",
                     content={
                         "msgtype": "m.text",
-                        "body": f"Done. {success_count}/{len(results)} succeeded 😶😶😶",
+                        "body": "Topic name invalid.",
                         "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
                     },
                 )
-            elif success_count == len(results):
-                await self.client.room_send(
-                    room.room_id,
-                    message_type="m.room.message",
-                    content={
-                        "msgtype": "m.text",
-                        "body": "Done. All succeeded 🎉🎉🎉",
-                        "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
-                    },
-                )
+                return
+
+        results: list[Process] = []
+        for package in packages:
+            logger.info("Updating %s", package)
+            p = await asyncio.create_subprocess_exec(
+                "python",
+                "-m",
+                "dcbot.update",
+                package,
+                topic,
+                "",
+                requester,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            results.append(p)
+
+        success_count = 0
+
+        for result in results:
+            await result.wait()
+            logger.info(result)
+            logger.info(type(result))
+            if result.returncode == 0:
+                success_count += 1
             else:
-                # unreachable
-                assert False
+                if result.stderr:
+                    logger.error(await result.stderr.read())
+
+        await mark_processed(event_id)
+
+        if success_count == 0:
+            await self.client.room_send(
+                room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": "All failed 😭😭😭",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+        elif success_count < len(results):
+            # 😶😶😶
+            await self.client.room_send(
+                room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": f"Done. {success_count}/{len(results)} succeeded 😶😶😶\n",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+        elif success_count == len(results):
+            await self.client.room_send(
+                room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": "Done. All succeeded 🎉🎉🎉",
+                    "m.relates_to": {"m.in_reply_to": {"event_id": event_id}},
+                },
+            )
+        else:
+            # unreachable
+            assert False
 
     async def run(self, timeout: int = 30000):
         self.client.add_event_callback(self.message_callback, RoomMessageText)
